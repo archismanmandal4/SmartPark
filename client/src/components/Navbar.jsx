1a3d6e83014efc925ccdcaa1bd5e88035a94669d
archismanmandal4 <archismanmandal3@gmail.com>
2026-08-08 23:06:25 +0530

    Fix mobile navbar layout

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,4 +1,3 @@
-
 import { Link, useNavigate, useLocation } from "react-router-dom";
 import { useState } from "react";
 import "./Navbar.css";
@@ -28,7 +27,6 @@ function Navbar() {
     localStorage.removeItem("user");
 
     setIsLoggedIn(false);
-
     navigate("/");
   };
 
@@ -40,105 +38,103 @@ function Navbar() {
 
   return (
     <nav className="smartpark-navbar">
-      <div className="navbar-inner">
 
-        {/* LOGO */}
-        <Link to="/" className="navbar-logo">
-          SmartPark
+      {/* LOGO */}
+      <Link to="/" className="navbar-logo">
+        SmartPark
+      </Link>
+
+      {/* NAVIGATION */}
+      <div className="navbar-links">
+
+        {/* HOME */}
+        <Link
+          to="/"
+          className={getLinkClass("/")}
+        >
+          Home
         </Link>
 
-        {/* NAVIGATION */}
-        <div className="navbar-links">
+        {/* MAP */}
+        <Link
+          to="/map"
+          className={getLinkClass("/map")}
+        >
+          Map
+        </Link>
 
-          {/* HOME */}
-          <Link
-            to="/"
-            className={getLinkClass("/")}
-          >
-            Home
-          </Link>
+        {/* HOW IT WORKS */}
+        <a
+          href="/#how-it-works"
+          className="navbar-link"
+        >
+          How It Works
+        </a>
 
-          {/* MAP */}
-          <Link
-            to="/map"
-            className={getLinkClass("/map")}
-          >
-            Map
-          </Link>
+        {/* LOGGED IN */}
+        {isLoggedIn ? (
+          <>
+            {/* DASHBOARD */}
+            <Link
+              to="/dashboard"
+              className={getLinkClass("/dashboard")}
+            >
+              Dashboard
+            </Link>
 
-          {/* HOW IT WORKS */}
-          <a
-            href="/#how-it-works"
-            className="navbar-link"
-          >
-            How It Works
-          </a>
-
-          {/* LOGGED IN */}
-          {isLoggedIn ? (
-            <>
-              {/* DASHBOARD */}
+            {/* OWNER */}
+            {user?.role === "owner" && (
               <Link
-                to="/dashboard"
-                className={getLinkClass("/dashboard")}
+                to="/add-parking"
+                className={getLinkClass("/add-parking")}
               >
-                Dashboard
+                Add Parking
               </Link>
+            )}
 
-              {/* OWNER */}
-              {user?.role === "owner" && (
-                <Link
-                  to="/add-parking"
-                  className={getLinkClass("/add-parking")}
-                >
-                  Add Parking
-                </Link>
-              )}
+            {/* USER NAME */}
+            <span className="navbar-user">
+              👋 {user?.name || "User"}
+            </span>
 
-              {/* USER NAME */}
-              <span className="navbar-user">
-                👋 {user?.name || "User"}
-              </span>
+            {/* BOOK SLOT */}
+            <button
+              type="button"
+              className="navbar-button"
+              onClick={() => navigate("/map")}
+            >
+              Book a Slot
+            </button>
 
-              {/* BOOK SLOT */}
-              <button
-                type="button"
-                className="navbar-button"
-                onClick={() => navigate("/map")}
-              >
-                Book a Slot
-              </button>
+            {/* LOGOUT */}
+            <button
+              type="button"
+              className="navbar-button navbar-logout"
+              onClick={handleLogout}
+            >
+              ↪ Logout
+            </button>
+          </>
+        ) : (
+          <>
+            {/* LOGIN */}
+            <Link
+              to="/login"
+              className={getLinkClass("/login")}
+            >
+              Login
+            </Link>
 
-              {/* LOGOUT */}
-              <button
-                type="button"
-                className="navbar-button navbar-logout"
-                onClick={handleLogout}
-              >
-                ↪ Logout
-              </button>
-            </>
-          ) : (
-            <>
-              {/* LOGIN */}
-              <Link
-                to="/login"
-                className={getLinkClass("/login")}
-              >
-                Login
-              </Link>
+            {/* REGISTER */}
+            <Link
+              to="/register"
+              className={getLinkClass("/register")}
+            >
+              Register
+            </Link>
+          </>
+        )}
 
-              {/* REGISTER */}
-              <Link
-                to="/register"
-                className={getLinkClass("/register")}
-              >
-                Register
-              </Link>
-            </>
-          )}
-
-        </div>
       </div>
     </nav>
   );
